47fd761491399581951988d3774dd73055f8e611
Jürgen Leschner <jurgen@jldec.me>
2026-07-01 13:54:44 -0500

    prettier etc.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from '@astrojs/cloudflare'
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,4 +12,4 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   }
-});
+})
